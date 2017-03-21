1340905b39b34d346f0620b0cab8a69936b03880
Tolecen <taox.l@163.com>
2017-03-21 13:13:31 +0800

    m

---
+++ UZLottery/Module/News/ZixunContent.h
@@ -0,0 +1,13 @@
+//
+//  ZixunContent.h
+//  UZLottery
+//
+//  Created by Xinle on 2017/3/21.
+//  Copyright © 2017年 com.uzero. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZixunContent : NSObject
++(NSArray *)array1;
+@end
